47c23f580501efcde4500246de587341b0982832
Nimo <nassimouali484@gmail.com>
2024-05-28 18:25:28 +0100

    attempting to fix db

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql')
 
 
 const db = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'chopain'
+    host: process.env.DB_SERVER,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 })
 
 db.getConnection((err, connection) => {
