875d64bc8d3cd2f511253fad9458898f2abc9153
Nimo <nassimouali484@gmail.com>
2024-05-30 00:45:42 +0100

    change

--- server.js
+++ server.js
@@ -28,14 +28,14 @@ app.use(session({
 app.use(cookieParser());
 
 app.use(cors({
-  origin: 'https://chopain-front.vercel.app , http://196.118.25.67', // Allow all origins
+  origin: 'http://localhost:5173', // Allow all origins
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 }));
 
 // Handle preflight requests
 app.options('*', cors({
-  origin: 'https://chopain-front.vercel.app , http://196.118.25.67', // Allow all origins
+  origin: 'http://localhost:5173', // Allow all origins
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
