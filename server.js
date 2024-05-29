e5222361ed088e1a4690d043c863a29212f52a3b
Nimo <nassimouali484@gmail.com>
2024-05-29 16:21:58 +0100

    cors

--- server.js
+++ server.js
@@ -28,15 +28,14 @@ app.use(session({
 app.use(cookieParser());
 
 app.use(cors({
-  origin: '*', // Allow all origins
+  origin: 'https://chopain-front.vercel.app/', // Allow all origins
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 // Handle preflight requests
 app.options('*', cors({
-  origin: '*', // Allow all origins
+  origin: 'https://chopain-front.vercel.app/', // Allow all origins
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
