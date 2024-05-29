f1e558cb5db7642849ab5343d2bc20c5c3744f8f
Nimo <nassimouali484@gmail.com>
2024-05-29 15:11:29 +0100

    Revert "attempting to fix db"
    This reverts commit 47c23f580501efcde4500246de587341b0982832.

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql')
 
 
 const db = mysql.createPool({
-    host: process.env.DB_SERVER,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'chopain'
 })
 
 db.getConnection((err, connection) => {
