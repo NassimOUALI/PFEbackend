37c8bbf72b88ea0d2e30d13ba67357fdb06ddb4a
Nimo <nassimouali484@gmail.com>
2024-05-30 15:16:00 +0100

    nn

--- server.js
+++ server.js
@@ -26,17 +26,17 @@ app.use(session({
   },
 }))
 
-app.use((req, res, next) => {
-  console.log(req.sessionID);
-  console.log(req.body.sessionid);
-  if (req.body.sessionid) {
-    req.sessionID = req.body.sessionid
-    next()
-  }
-  {
-    // req.sessionid = generated
-  }
-})
+// app.use((req, res, next) => {
+//   console.log(req.sessionID);
+//   console.log(req.body.sessionid);
+//   if (req.body.sessionid) {
+//     req.sessionID = req.body.sessionid
+//     next()
+//   }
+//   {
+//     // req.sessionid = generated
+//   }
+// })
 
 
 app.use(cookieParser());
