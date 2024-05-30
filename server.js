e70b0bf616dcf62758a1493a4827ffadeebe1eab
Nimo <nassimouali484@gmail.com>
2024-05-30 15:37:44 +0100

    httponly

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    httpOnly: true, secure: true, maxAge: 1000 * 60 * 60 * 48
+    secure: true, maxAge: 1000 * 60 * 60 * 48
   },
 }))
 
